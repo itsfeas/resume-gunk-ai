da8996acd0e3b7f1feefcc1a0dfd13782307990a
itsfeas <amf.fiaz@gmail.com>
2024-01-30 21:14:24 -0700

    genetic algorithm for finding optimal NN structure

--- src/ai/resume_split/genetic.py
+++ src/ai/resume_split/genetic.py
@@ -18,7 +18,7 @@ for i, resume_path in enumerate(PDFS):
 df = pd.concat(tot_features, axis=0, ignore_index=True)
 
 # Random Sampling
-df = random_sampling_remove_null(df, 0.7)
+df = random_sampling_remove_null(df, 0.8)
 
 #Split data into a training and testing set
 Y = df['Labels'].values
@@ -26,25 +26,27 @@ X = df.drop(labels=['Labels'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=19)
 
 # Function to create and train the neural network
-def create_nn(hidden_layers, neurons_per_layer):
-    print(hidden_layers, neurons_per_layer)
-    if hidden_layers == 0 or neurons_per_layer == 0:
-        return 0
-    model = MLPClassifier(random_state=1, early_stopping=True, max_iter=2500, hidden_layer_sizes=(neurons_per_layer,) * hidden_layers)
+def create_nn(hidden_layers):
+    print(hidden_layers)
+    for h in hidden_layers:
+        if h == 0:
+            return 0
+    model = MLPClassifier(random_state=1, early_stopping=True, max_iter=10000, hidden_layer_sizes=hidden_layers)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
+    
     accuracy = accuracy_score(y_test, y_pred)
     return accuracy
 
 # Define the fitness function for the genetic algorithm
 def fitness(individual, data):
-    hidden_layers, neurons_per_layer = individual
-    accuracy = create_nn(hidden_layers, neurons_per_layer)
+    hidden_layers = individual
+    accuracy = create_nn(hidden_layers)
     return accuracy
 
 # Define the genetic algorithm parameters
 population_size = 10
-generations = 5
+generations = 10
 mutation_probability = 0.2
 
 # Create a genetic algorithm instance
@@ -53,7 +55,7 @@ ga = pyeasyga.GeneticAlgorithm(seed_data=None, population_size=population_size, 
                       elitism=True, maximise_fitness=True)
 
 # Define the population
-ga.create_individual = lambda _: [np.random.randint(400, 1500) for _ in range(np.random.randint(2, 5))]
+ga.create_individual = lambda _: [np.random.randint(400, 2000) for _ in range(np.random.randint(2, 7))]
 ga.fitness_function = fitness
 
 # Run the genetic algorithm

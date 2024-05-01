cfbe716b79a527894d2c89ce7fe7c1363b58190b
itsfeas <amf.fiaz@gmail.com>
2024-05-01 16:19:54 -0600

    heuristic work

--- src/ai/format_test/heuristic.py
+++ src/ai/format_test/heuristic.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from functools import cache
+from os import walk
 from os.path import join, dirname
 from dotenv import load_dotenv
 import string
@@ -27,5 +28,63 @@ import re
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import cv2
-from pdf2image import convert_from_path
+import visualize_pages
 
+def list_files(mypath):
+	f = []
+	for (dirpath, dirnames, filenames) in walk(mypath):
+		f.extend(filenames)
+		break
+	return(f)
+
+def convert_img(img_path: str):
+	img = cv2.imread(img_path)
+	cv2.imshow("image", img)
+	# img_row_expand = img.copy()
+	# for i in range(img_row_expand.shape[0]):
+	# 	if np.any(img_row_expand[i] != [255, 255, 255]):
+	# 		img_row_expand[i] = [0, 0, 0]
+	img_column_expand = img.copy()
+	for j in range(img.shape[1]):
+		if np.any(img[:, j] != [255, 255, 255]):
+			img_column_expand[:, j] = [0, 0, 0]
+	cv2.imshow("img_column_expand", img_column_expand)
+	
+	img_expand = img.copy()
+	dil_size = 1
+	dil_element = cv2.getStructuringElement(cv2.MORPH_DILATE, (2 * dil_size + 1, 2 * dil_size + 1), (dil_size, dil_size))
+
+	img_expand = cv2.cvtColor(img_expand, cv2.COLOR_BGR2GRAY)
+	img_expand = cv2.equalizeHist(img_expand)
+	img_expand = cv2.inRange(img_expand, (0), (125))
+	img_expand = cv2.dilate(img_expand, dil_element)
+	img_expand = cv2.inRange(img_expand, (0), (125))
+	cv2.imshow("img_expand", img_expand)
+	cv2.waitKey(0)
+
+def get_header_b_boxes(path: str):
+	params = LAParams(char_margin=200, line_margin=1)
+	pages = extract_pages(path, laparams=params)
+	for page in pages:
+		# page.analyze(params)
+		visualize_pages.show_ltitem_hierarchy(page)
+	
+if __name__ == "__main__":
+	pdf_path = "./dataset/"
+	img_path = "./dataset/img/"
+	file_name = "train_resume1.pdf"
+	traverse = False
+	traverse = True
+	max_files = 5
+	if traverse:
+		pdf_files = list_files(pdf_path)
+		for file in pdf_files[:max_files]:
+			bboxes = get_header_b_boxes(pdf_path+file)
+			# print(bboxes)
+		# img_files = list_files(img_path)[:max_files]
+		# for file in img_files:
+		# 	convert_img(img_path+file)
+	else:
+		bboxes = get_header_b_boxes(pdf_path+file_name)
+		convert_img(img_path+file_name.replace(".pdf", "_0.pdf"))
+	cv2.destroyAllWindows()
